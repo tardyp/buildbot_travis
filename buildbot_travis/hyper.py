0ca9571f2761f47514a5347e7b60270fe388fc11
Pierre Tardy <tardyp@gmail.com>
2016-07-12 13:03:11 +0200

    add configuration for size

--- buildbot_travis/hyper.py
+++ buildbot_travis/hyper.py
@@ -18,8 +18,10 @@ from __future__ import absolute_import
 
 import socket
 
+from twisted.internet import reactor as global_reactor
 from twisted.internet import defer, threads
 from twisted.python import log
+from twisted.python.threadpool import ThreadPool
 
 from buildbot import config
 from buildbot.interfaces import LatentWorkerFailedToSubstantiate
@@ -39,6 +41,7 @@ def _handle_stream_line(line):
     Input is the json representation of: {'stream': "Content\ncontent"}
     Output is a generator yield "Content", and then "content"
     """
+
     # XXX This necessary processing is probably a bug from docker-py,
     # hence, might break if the bug is fixed, i.e. we should get decoded JSON
     # directly from the API.
@@ -54,13 +57,13 @@ def _handle_stream_line(line):
 
 class HyperLatentWorker(AbstractLatentWorker):
     instance = None
+    ALLOWED_SIZES = ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl', 'xxl']
+    threadPool = None
+    client = None
+    reactor = global_reactor
 
-    def __init__(self, name, password, image, hyper_host,
-                 hyper_accesskey, hyper_secretkey, masterFQDN=None, **kwargs):
-
-        if not Hyper:
-            config.error("The python modules 'docker-py>=1.4' and 'hypercompose' are needed to use a"
-                         " HyperLatentWorker")
+    def checkConfig(self, name, password, image, hyper_host,
+                    hyper_accesskey, hyper_secretkey, hyper_size="xs", masterFQDN=None, **kwargs):
 
         # Set build_wait_timeout to 10 if not explicitely set: Starting a
         # container is almost immediate, we can affort doing so for each build.
@@ -68,9 +71,22 @@ class HyperLatentWorker(AbstractLatentWorker):
         if 'build_wait_timeout' not in kwargs:
             kwargs['build_wait_timeout'] = 10
 
-        AbstractLatentWorker.__init__(self, name, password, **kwargs)
+        AbstractLatentWorker.checkConfig(self, name, password, **kwargs)
 
+        if not Hyper:
+            config.error("The python modules 'docker-py>=1.4' and 'hypercompose' are needed to use a"
+                         " HyperLatentWorker")
+
+        if hyper_size not in self.ALLOWED_SIZES:
+            config.error("Size is not valid %s vs %r".format(hyper_size, self.ALLOWED_SIZED))
+
+    def reconfigService(self, name, password, image, hyper_host,
+                        hyper_accesskey, hyper_secretkey, hyper_size="xs", masterFQDN=None, **kwargs):
+
+        AbstractLatentWorker.reconfigService(self, name, password, **kwargs)
+        self.size = hyper_size
         self.image = image
+
         # Prepare the parameters for the Docker Client object.
         self.client_args = {'clouds': {
             hyper_host: {
@@ -78,9 +94,20 @@ class HyperLatentWorker(AbstractLatentWorker):
                 "secretkey": hyper_secretkey
             }
         }}
-        if not masterFQDN:
+        if not masterFQDN:  # also match empty string (for UI)
             masterFQDN = socket.getfqdn()
         self.masterFQDN = masterFQDN
+
+    @defer.inlineCallbacks
+    def stopService(self):
+        # stopService will call stop_instance if the worker was up.
+        yield AbstractLatentWorker.stopService(self)
+
+        # we cleanup our thread and session (or reactor.stop will hang)
+        self.client.close()
+        self.client = None
+        yield self.threadPool.stop()
+        self.threadPool = None
 
     def createEnvironment(self):
         result = {
@@ -90,6 +117,9 @@ class HyperLatentWorker(AbstractLatentWorker):
         }
         if self.registration is not None:
             result["BUILDMASTER_PORT"] = str(self.registration.getPBPort())
+
+        result["BUILDMASTER_PORT"] = '10975'
+        result["BUILDMASTER"] = '0.tcp.ngrok.io'
         print result
         return result
 
@@ -97,30 +127,27 @@ class HyperLatentWorker(AbstractLatentWorker):
     def start_instance(self, build):
         if self.instance is not None:
             raise ValueError('instance active')
+
+        if not self.threadPool:
+            # requests_aws4 is documented to not be thread safe, so we must serialize access
+            self.threadPool = ThreadPool(minthreads=1, maxthreads=1, name='hyper')
+            self.threadPool.start()
+
+        if not self.client:
+            self.client = Hyper(self.client_args)
+
         image = yield build.render(self.image)
-        res = yield threads.deferToThread(self._thd_start_instance, image)
+        res = yield threads.deferToThreadPool(self.reactor, self.threadPool, self._thd_start_instance, image)
         defer.returnValue(res)
 
-    def _image_exists(self, client, name):
-        # Make sure the image exists
-        for image in client.images():
-            for tag in image['RepoTags']:
-                if ':' in name and tag == name:
-                    return True
-                if tag.startswith(name + ':'):
-                    return True
-        return False
-
     def _thd_start_instance(self, image):
-        hyper_client = Hyper(self.client_args)
-        instance = hyper_client.create_container(
+        instance = self.client.create_container(
             image,
-            name='%s%s' % (self.workername, id(self)),
+            name=('%s%s' % (self.workername, id(self))).replace("_", "-"),
             environment=self.createEnvironment(),
         )
 
         if instance.get('Id') is None:
-            log.msg('Failed to create the container')
             raise LatentWorkerFailedToSubstantiate(
                 'Failed to start container'
             )
@@ -128,8 +155,7 @@ class HyperLatentWorker(AbstractLatentWorker):
         log.msg('Container created, Id: %s...' % (shortid,))
         instance['image'] = image
         self.instance = instance
-        hyper_client.start(instance)
-        log.msg('Container started')
+        self.client.start(instance)
         return [instance['Id'], image]
 
     def stop_instance(self, fast=False):
@@ -140,12 +166,13 @@ class HyperLatentWorker(AbstractLatentWorker):
             return defer.succeed(None)
         instance = self.instance
         self.instance = None
-        return threads.deferToThread(self._thd_stop_instance, instance, fast)
+        return threads.deferToThreadPool(self.reactor, self.threadPool,
+                                         self._thd_stop_instance, instance, fast)
 
     def _thd_stop_instance(self, instance, fast):
-        hyper_client = Hyper(self.client_args)
+        self.client = Hyper(self.client_args)
         log.msg('Stopping container %s...' % instance['Id'][:6])
-        hyper_client.stop(instance['Id'])
+        self.client.stop(instance['Id'])
         if not fast:
-            hyper_client.wait(instance['Id'])
-        hyper_client.remove_container(instance['Id'], v=True, force=True)
+            self.client.wait(instance['Id'])
+        self.client.remove_container(instance['Id'], v=True, force=True)
